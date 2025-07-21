8b1bd9498d4509c3a0c99e3a11ff54f64dae4d10
cispe <cispe@CISPE-DESKTOP>
2025-07-21 15:17:58 +0300

    'MSTest' project was added

---
+++ CSharpFantaMentos13.MSTest/CirtusTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using CSharpFantaMentos13.View1;
+
+namespace CSharpFantaMentos13.MSTest;
+
+[TestClass]
+public sealed class CitrusTest
+{
+    [TestMethod] public void ConstructorTest()
+    {
+        Citrus citrus = new("Lemon", "Yellow", 5);
+        Assert.AreEqual("Lemon", citrus.Name, $"Constructor initialize {nameof(Citrus)}.{nameof(Citrus.Name)} incorrectly");
+        Assert.AreEqual("Yellow", citrus.Color, $"Constructor initialize {nameof(Citrus)}.{nameof(Citrus.Color)} incorrectly");
+        Assert.AreEqual(5, citrus.VitaminC, $"Constructor initialize {nameof(Citrus)}.{nameof(Citrus.VitaminC)} incorrectly");
+        Assert.ThrowsException<ArgumentOutOfRangeException>(delegate
+        {
+            Citrus citrus = new("", "", -5);
+        }, $"Constructor must throw an '{nameof(ArgumentOutOfRangeException)}' when 'vitamin_c' parameter is negative");
+    }
+    [TestMethod] public void PropertiesTest()
+    {
+        Citrus citrus = new("", "", 0)
+        {
+            Name = "Lemon",
+            Color = "Yellow",
+            VitaminC = 5,
+        };
+        Assert.AreEqual("Lemon", citrus.Name, $"{nameof(Citrus)}.{nameof(Citrus.Name)} get/set incorrect");
+        Assert.AreEqual("Yellow", citrus.Color, $"{nameof(Citrus)}.{nameof(Citrus.Color)} get/set incorrect");
+        Assert.AreEqual(5, citrus.VitaminC, $"{nameof(Citrus)}.{nameof(Citrus.VitaminC)} get/set incorrect");
+        Assert.ThrowsException<ArgumentOutOfRangeException>(delegate
+        {
+            citrus.VitaminC = -5;
+        }, $"{nameof(Citrus)}.{nameof(Citrus.VitaminC)} set must throw an '{nameof(ArgumentOutOfRangeException)}' when 'value' parameter is negative");
+    }
+    [TestMethod] public void InputMethodTest()
+    {
+        Console.SetIn(new StringReader($"Lemon{Environment.NewLine}Yellow{Environment.NewLine}5{Environment.NewLine}"));
+        Citrus citrus = new("", "", 0);
+        citrus.Input();
+        Assert.AreEqual("Lemon", citrus.Name, $"void {nameof(Citrus)}.{nameof(Citrus.Input)}() initialize {nameof(Citrus)}.{nameof(Citrus.Name)} incorrectly");
+        Assert.AreEqual("Yellow", citrus.Color, $"void {nameof(Citrus)}.{nameof(Citrus.Input)}() initialize {nameof(Citrus)}.{nameof(Citrus.Color)} incorrectly");
+        Assert.AreEqual(5, citrus.VitaminC, $"void {nameof(Citrus)}.{nameof(Citrus.Input)}() initialize {nameof(Citrus)}.{nameof(Citrus.VitaminC)} incorrectly");
+        Assert.ThrowsException<ArgumentOutOfRangeException>(delegate
+        {
+            Console.SetIn(new StringReader($"Lemon{Environment.NewLine}Yellow{Environment.NewLine}-5{Environment.NewLine}"));
+            Citrus citrus = new("", "", 0);
+            citrus.Input();
+        }, $"{nameof(Citrus)}.{nameof(Citrus.VitaminC)} set must throw an '{nameof(ArgumentOutOfRangeException)}' when 'value' parameter is negative");
+    }
+    [TestMethod] public void OutputMethodTest()
+    {
+        StringBuilder builder = new();
+        Console.SetOut(new StringWriter(builder));
+        Citrus citrus = new("Lemon", "Yellow", 5);
+        citrus.Output();
+        Assert.AreEqual($"{nameof(Fruit.Name)}: {citrus.Name}; {nameof(Fruit.Color)}: {citrus.Color}; {nameof(Citrus.VitaminC)}: {citrus.VitaminC}{Environment.NewLine}", builder.ToString(), $"void {nameof(Citrus)}.{nameof(Citrus.Output)}() works incorrectly");
+    }
+    [TestMethod] public void ToStringMethodTest()
+    {
+        Citrus citrus = new("Lemon", "Yellow", 5);
+        Assert.AreEqual($"{nameof(Fruit.Name)}: {citrus.Name}; {nameof(Fruit.Color)}: {citrus.Color}; {nameof(Citrus.VitaminC)}: {citrus.VitaminC}", citrus.ToString(), "string ToString() returns invalid value");
+
+        Fruit fruit = citrus;
+        Assert.AreEqual($"{nameof(Fruit.Name)}: {citrus.Name}; {nameof(Fruit.Color)}: {citrus.Color}; {nameof(Citrus.VitaminC)}: {citrus.VitaminC}", fruit.ToString(), "string ToString() must override Fruit.ToString()");
+    }
+}
---
+++ CSharpFantaMentos13.MSTest/FruitTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using CSharpFantaMentos13.View1;
+
+namespace CSharpFantaMentos13.MSTest;
+
+[TestClass]
+public sealed class FruitTest
+{
+    [TestMethod] public void ConstructorTest()
+    {
+        Fruit fruit = new("Apple", "Red");
+        Assert.AreEqual("Apple", fruit.Name, $"Constructor initialize {nameof(Fruit)}.{nameof(Fruit.Name)} incorrectly");
+        Assert.AreEqual("Red", fruit.Color, $"Constructor initialize {nameof(Fruit)}.{nameof(Fruit.Color)} incorrectly");
+    }
+    [TestMethod] public void PropertiesTest()
+    {
+        Fruit fruit = new("", "")
+        {
+            Name = "Apple",
+            Color = "Red",
+        };
+        Assert.AreEqual("Apple", fruit.Name, $"{nameof(Fruit)}.{nameof(Fruit.Name)} get/set incorrect");
+        Assert.AreEqual("Red", fruit.Color, $"{nameof(Fruit)}.{nameof(Fruit.Color)} get/set incorrect");
+    }
+    [TestMethod] public void InputMethodTest()
+    {
+        Console.SetIn(new StringReader($"Apple{Environment.NewLine}Red{Environment.NewLine}"));
+        Fruit fruit = new("", "");
+        fruit.Input();
+        Assert.AreEqual("Apple", fruit.Name, $"void {nameof(Fruit)}.{nameof(Fruit.Input)}() initialize {nameof(Fruit)}.{nameof(Fruit.Name)} incorrectly");
+        Assert.AreEqual("Red", fruit.Color, $"void {nameof(Fruit)}.{nameof(Fruit.Input)}() initialize {nameof(Fruit)}.{nameof(Fruit.Color)} incorrectly");
+    }
+    [TestMethod] public void OutputMethodTest()
+    {
+        StringBuilder builder = new();
+        Console.SetOut(new StringWriter(builder));
+        Fruit fruit = new("Apple", "Red");
+        fruit.Output();
+        Assert.AreEqual($"{nameof(Fruit.Name)}: {fruit.Name}; {nameof(Fruit.Color)}: {fruit.Color}{Environment.NewLine}", builder.ToString(), $"void {nameof(Fruit)}.{nameof(Fruit.Output)}() works incorrectly");
+    }
+    [TestMethod] public void ToStringMethodTest()
+    {
+        Fruit fruit = new("Apple", "Red");
+        Assert.AreEqual($"{nameof(Fruit.Name)}: {fruit.Name}; {nameof(Fruit.Color)}: {fruit.Color}", fruit.ToString(), "string ToString() returns invalid value");
+
+        object obj = fruit;
+        Assert.AreEqual($"{nameof(Fruit.Name)}: {fruit.Name}; {nameof(Fruit.Color)}: {fruit.Color}", obj.ToString(), "string ToString() must override object.ToString()");
+    }
+    [TestMethod] public void CompareMethodTest()
+    {
+        Fruit fruit1 = new("Apple", "Red");
+        Fruit fruit2 = new("Banana", "Yellow");
+        Fruit fruit3 = new("Apple", "Green");
+
+        Assert.AreEqual(-1, fruit1.CompareTo(fruit2), $"int CompareTo({nameof(Fruit)} other) must compare object by {nameof(Fruit.Name)} property");
+        Assert.AreEqual(1, fruit2.CompareTo(fruit1), $"int CompareTo({nameof(Fruit)} other) must compare object by {nameof(Fruit.Name)} property");
+        Assert.AreEqual(0, fruit1.CompareTo(fruit3), $"int CompareTo({nameof(Fruit)} other) must compare object by {nameof(Fruit.Name)} property");
+    }
+}
