6ebd52d8ddda25d4b00a9efedfb164f7d2434f6d
cispe <cispe@CISPE-DESKTOP>
2025-07-21 10:48:28 +0300

    Add project files.

---
+++ CSharpFantaMentos13/View1/Citrus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpFantaMentos13.View1;
+
+public sealed class Citrus : Fruit
+{
+    #region Instance
+    private int vitamin_c;
+    public int VitaminC
+    {
+        get => vitamin_c;
+        set
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(value, nameof(value));
+            vitamin_c = value;
+        }
+    }
+
+    public Citrus(string name, string color, int vitamin_c) : base(name, color)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(vitamin_c, nameof(vitamin_c));
+        this.vitamin_c = vitamin_c;
+    }
+    #endregion
+
+    #region Fruit
+    public override void Input()
+    {
+        base.Input();
+        VitaminC = Fruit.GetValueFromConsole<int>($"{nameof(Fruit)}.{nameof(VitaminC)}");
+    }
+    public override void Output()
+    {
+        Console.WriteLine(this.ToString());
+    }
+    #endregion
+
+    #region Object
+    public override string ToString()
+    {
+        return $"{base.ToString()}; {nameof(VitaminC)}: {VitaminC}";
+    }
+    #endregion
+}
---
+++ CSharpFantaMentos13/View1/Fruit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace CSharpFantaMentos13.View1;
+
+public class Fruit
+{
+    #region Static
+    static internal T GetValueFromConsole<T>(string message) where T : IParsable<T>
+    {
+        T? value;
+        Console.Write($"{message}({typeof(T).FullName})>>> ");
+        while(T.TryParse(Console.ReadLine() ?? "", CultureInfo.InvariantCulture, out value) is false)
+        {
+            Console.Write("Can`t parse input. Try again: ");
+            Console.Write($"{message}({typeof(T).FullName})>>> ");
+        }
+        return value;
+    }
+    #endregion
+
+    #region Instance
+    private string name;
+    public string Name
+    {
+        get => name;
+        set
+        {
+            name = value;
+        }
+    }
+
+    private string color;
+    public string Color
+    {
+        get => color;
+        set
+        {
+            color = value;
+        }
+    }
+
+    public Fruit(string name, string color)
+    {
+        this.name = name;
+        this.color = color;
+    }
+
+    public virtual void Input()
+    {
+        Name = GetValueFromConsole<string>($"{nameof(Fruit)}.{nameof(Name)}");
+        Color = GetValueFromConsole<string>($"{nameof(Fruit)}.{nameof(Color)}");
+    }
+    public virtual void Output()
+    {
+        Console.WriteLine(this.ToString());
+    }
+    #endregion
+
+    #region Object
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}; {nameof(Color)}: {Color}";
+    }
+    #endregion
+}
